f53afcd175b7c39e4a9f201c17fe494141e84f41
Ashley Schultz <ashleyeschultz@gmail.com>
2018-07-06 17:20:20 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,10 +2,10 @@ class Song < ActiveRecord::Base
   belongs_to :artist
 
   def artist_name
+    self.artist ? self.artist.name : nil
   end
 
   def artist_name=(name)
+    self.artsit = Artist.find_or_create_by(name: name)
   end
-
-#self.artist ? self.artist.name : nil
 end
