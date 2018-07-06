b705c19ed4ce4baa1257ab33463ba3643738422e
Ashley Schultz <ashleyeschultz@gmail.com>
2018-07-06 17:29:56 +0000

    automatically backed up by learn

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -1,2 +1,10 @@
 module ArtistsHelper
+
+  def display_artist
+    if self.artist_name
+      link_to artist_name, artist_path(self.artist)
+    else 
+      link_to 'Add Artist', edit_song_path(self)
+  end
+  
 end
